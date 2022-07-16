8d1798717bceadb8f53f15cff3f23e9f8c750b3a
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-16 18:36:01 +0800

    Setup `database` function

--- activate_code_server/server.py
+++ activate_code_server/server.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 
+from .db import database
+
 class app_server:
     def __init__(self, *args, **kwargs):
-        self.port = kwargs.get('port', 8000)
         self.host = kwargs.get('host', 'localhost')
+        self.port = kwargs.get('port', 8000)
         self.database_engine = kwargs.get('database_engine', 'sqlite')
+        self.database_filename = kwargs.get('database_filename', 'database')
+        self.database_host = kwargs.get('database_host', '127.0.0.1')
+        self.database_port = kwargs.get('database_port')
         self.database_name = kwargs.get('database_name', 'activate_code_server')
         self.database_user = kwargs.get('database_user', 'root')
         self.database_password = kwargs.get('database_password', '12345678')
         self.app = None
         self.server = None
+        self.database = None
         self.setup_app()
+        self.setup_database()
         
     def setup_app(self):
         self.app = Flask(__name__)
@@ -23,7 +30,19 @@ class app_server:
         self.server = WSGIServer(
             (self.host, self.port),
             self.app)
-
+    
+    def setup_database(self):
+        db = database(
+            database_engine=self.database_engine,
+            database_filename=self.database_filename,
+            database_host=self.database_host,
+            database_port=self.database_port,
+            database_name=self.database_name,
+            database_user=self.database_user,
+            database_password=self.database_password
+        )
+        self.database = db.database
+        
     def run(self):
         self.setup_server()
         self.server.serve_forever()
