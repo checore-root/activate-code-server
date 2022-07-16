a4ad8933834ec4620214ab77b48e94b2bf41be2b
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-16 15:51:13 +0800

    Remove testing function

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import activate_code_server
 
 server = activate_code_server.app_server(
-    debug=False
+    host='localhost',
+    port=7000,
 )
-from flask import Blueprint
-
-
-simple_page = Blueprint('simple_page', __name__, template_folder='templates')
-
-@simple_page.route('/')
-def show():
-    return 'Hello World!'
 
 if __name__ == "__main__":
-    server.setup_blueprint(simple_page)
-    server.setup_server()
+    server.setup_blueprint(activate_code_server.simple_page)
     server.run()
