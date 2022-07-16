301cd86905254df02752c35c8be616528be342cb
cheehong <71654184+cheehong1030@users.noreply.github.com>
2022-07-16 16:18:17 +0800

    Add `database` option

--- activate_code_server/server.py
+++ activate_code_server/server.py
@@ -5,6 +5,10 @@ class app_server:
     def __init__(self, *args, **kwargs):
         self.port = kwargs.get('port', 8000)
         self.host = kwargs.get('host', 'localhost')
+        self.database_engine = kwargs.get('database_engine', 'sqlite')
+        self.database_name = kwargs.get('database_name', 'activate_code_server')
+        self.database_user = kwargs.get('database_user', 'root')
+        self.database_password = kwargs.get('database_password', '12345678')
         self.app = None
         self.server = None
         self.setup_app()
